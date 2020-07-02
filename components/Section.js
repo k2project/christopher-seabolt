d578387c7698c8aeecdff9737d67b7d0275ab060
k2projct <kopczkrzy@gmail.com>
2020-07-02 14:25:39 +0100

    comment removed: git merge conflict exercise

--- components/Section.js
+++ components/Section.js
@@ -6,7 +6,6 @@ export default function Section({ cls, children }) {
         let top = img.current.parentElement.getBoundingClientRect().top;
         img.current.style.top = -top * 0.3 + 'px';
     };
-    //working with git
     useEffect(() => {
         window.addEventListener('scroll', onScroll);
         return () => {
