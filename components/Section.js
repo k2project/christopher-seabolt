4d7ce7a76a54c92ee61aed5d4a21f83dd98cbbca
k2projct <kopczkrzy@gmail.com>
2020-12-25 17:53:22 +0000

    updates 1

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useRef } from 'react';
 
-export default function Section({ cls, children }) {
+export default function Section({ cls, header, children }) {
     const img = useRef(null);
     const onScroll = () => {
         let top = img.current.parentElement.getBoundingClientRect().top;
@@ -17,23 +17,10 @@ export default function Section({ cls, children }) {
         <Fragment>
             <section className={'paralex ' + cls}>
                 <div className='paralex-img' ref={img}></div>
-                <div className='paralex-cover'>{children}</div>
+                <div className='paralex-cover'>{header}</div>
             </section>
             <div className='filler'>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Excepteur sint occaecat cupidatat non proident, sunt
-                    in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <p>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.
-                </p>
+                {children}
             </div>
         </Fragment>
     );
--- pages/index.js
+++ pages/index.js
@@ -14,32 +14,59 @@ export default function Home() {
             </Head>
             <Header />
             <main>
-                <Section cls='top'>
-                    <h2 className='paralex-title'>Business like no others</h2>
+                <Section cls='top' header={<> <h2 className='paralex-title'>Early-stage Venture <br/>Gamechanger</h2>
                     <p className='paralex-subtitle'>
-                        You can say what you want here. Just sell it!
-                    </p>
-                    <div className='btn-box'>
-                        <button className='btn btn--theme'>Take action</button>
-                    </div>
+                    Top 5% Industry Performance, Returns Weighted.
+                    </p></>}>
+                    <h2>Venture Capital Professional - Generalist Technology, Specialist Early Stage</h2>
+                <p>
+                        Christopher holds over 30 years of venture capital and operational experience in early-stage growth companies.  
+                </p>
+                    <p>Of almost 20 deals across multiple funds in the early-stage sector, consisting entirely of Seed and Start-up, Christopher has engineered successful (+money) exits for well over half of these, with extraordinarily high returns across all managed portfolios. </p>
+                    <p>This coveted position was achieved through a combination of technical financial and investment skills, combined with broad and deep management experience well beyond that typically available to early-stage companies and founder teams. </p>
+               
                 </Section>
-                <Section cls='middle'>
-                    <h2 className='paralex-title'>Navigate to Success</h2>
+                <Section cls='middle' header={<> <h2 className='paralex-title'>Navigate to Success</h2>
                     <p className='paralex-subtitle'>
-                        You can say what you want here. Just sell it!
+                    Drive rapid scale to enhanced exit returns.
                     </p>
-                    <div className='btn-box'>
+                    {/* <div className='btn-box'>
                         <button className='btn btn--theme'>Other Action</button>
-                    </div>
+                    </div> */}
+                </>
+                }>
+                    <h2> No company failed because things went according to plan. </h2>
+                    <p> The world of and approach to early-stage venture investing ceased to make pragmatic sense decades ago.  </p>
+                    <p>Realized returns are fundamental but seem largely absent from focus. The industry is driven by a focus on PR and not IRR. There is no other financial sector where achievable returns are as of little relevance. This is not sustainable. </p>
+                    <p>
+                    There is a misconception that if you provide capital to early-stage companies, they are positioned to succeed. Pragmatic execution strategies on deployed capital are the primary failure point in early-stage. Venture investors at these stages must not only provide capital but bridge operational and executional competences. 
+                    </p>
+
+                    <p>When the combination of these competences have been brought together and made available to early-stage companies, Christopher delivered market leading results across a broad spectrum of industries. </p>
+
+
+                    <ul>
+                        <li>Business plans must reflect pragmatic go-to-market strategies, with target markets of sufficient size to accommodate early-stage risk. </li>
+                        <li>Products must be developed to pragmatic market needs. There are too many solutions looking for a problem, and far too many overengineered solutions predicated on the technology aspects of the product enhancing price relative to simpler, less engineering intensive and lower cost alternatives. </li>
+                        <li>Execution strategies within these target markets must be pragmat</li>
+                        <li>Execution strategies within these target markets must be pragmatic in terms of achievable results within limited cash and human resource constraints. </li>
+                        <li>Operational infrastructures must be pragmatically large enough to achieve target go-to-market execution goals, while pragmatically small enough to maximize benefit across limited resources. </li>
+                    </ul>
+
+                    <p>Early-stage companies usually lack these competences in house. Expecting successful capital deployment with this lacking isn’t pragmatic. Expecting good ideas to build multi-million-dollar revenue streams in and of themselves isn’t pragmatic. </p>
+                    <p><b>“It’s in the execution of the business model, stupid…”</b></p>
+                    
+                   
                 </Section>
-                <Section cls='bottom'>
-                    <h2 className='paralex-title'>Stay Connected</h2>
+                <Section cls='bottom' header={<>
+                    <h2 className='paralex-title'>Let's talk business!</h2>
                     <p className='paralex-subtitle'>
                         You can say what you want here. Just sell it!
                     </p>
-                    <div className='btn-box'>
+                    {/* <div className='btn-box'>
                         <button className='btn btn--theme'>Take action</button>
-                    </div>
+                    </div> */}
+                </>}>
                 </Section>
             </main>
             <Footer />
