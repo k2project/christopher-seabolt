13311349382cda959ad9311215387bcd7da12627
k2projct <kopczkrzy@gmail.com>
2020-12-26 13:25:35 +0000

    chris photo added

--- components/sections/About.js
+++ components/sections/About.js
@@ -15,30 +15,39 @@ const Header = (
 export default function About() {
     return (
         <Section cls='top' header={Header}>
-            <div className='wrapper wrapper-w-padding' id='who-I-am'>
-                <h2></h2>
-                <h3 className='section-subtitle'>
-                    Venture Capital Professional - Generalist Technology,
-                    Specialist Early-stage.
-                </h3>
-                <p>
-                    Christopher holds over 30 years of venture capital and
-                    operational experience in early-stage growth companies.
-                </p>
-                <p>
-                    Of almost 20 deals across multiple funds in the early-stage
-                    sector, consisting entirely of Seed and Start-up,
-                    Christopher has engineered successful (+money) exits for
-                    well over half of these, with extraordinarily high returns
-                    across all managed portfolios.
-                </p>
-                <p>
-                    This coveted position was achieved through a combination of
-                    technical financial and investment skills, combined with
-                    broad and deep management experience well beyond that
-                    typically available to early-stage companies and founder
-                    teams.
-                </p>
+            <div className='wrapper'>
+                <div className='two-columns'>
+                    <div className='about' id='who-I-am'>
+                        <h2></h2>
+                        <h3 className='section-subtitle'>
+                            Venture Capital Professional - Generalist
+                            Technology, Specialist Early-stage.
+                        </h3>
+                        <p>
+                            Christopher holds over 30 years of venture capital
+                            and operational experience in early-stage growth
+                            companies.
+                        </p>
+                        <p>
+                            Of almost 20 deals across multiple funds in the
+                            early-stage sector, consisting entirely of Seed and
+                            Start-up, Christopher has engineered successful
+                            (+money) exits for well over half of these, with
+                            extraordinarily high returns across all managed
+                            portfolios.
+                        </p>
+                        <p>
+                            This coveted position was achieved through a
+                            combination of technical financial and investment
+                            skills, combined with broad and deep management
+                            experience well beyond that typically available to
+                            early-stage companies and founder teams.
+                        </p>
+                    </div>
+                    <div className='chris-img'>
+                        <div className='caption'>Christopher Seabolt</div>
+                    </div>
+                </div>
             </div>
         </Section>
     );
